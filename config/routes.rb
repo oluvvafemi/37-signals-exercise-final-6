d1b59cbfcdc84c753118b90ae020781f9bc6dbdd
Chris Adebiyi <oluvvafemi@gmail.com>
2025-05-17 18:25:08 +0200

    Setup index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :web_pages, only: [ :index ]
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "web_pages#index"
   resources :mockups if Rails.env.development?
 end
