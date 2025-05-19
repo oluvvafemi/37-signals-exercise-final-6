e2c14cb970f83cdebf01bb4103322103dab9a9e4
Chris Adebiyi <oluvvafemi@gmail.com>
2025-05-19 21:05:05 +0200

    Add retry and update model relationship

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,14 +1,19 @@
 class AnalysesController < ApplicationController
   before_action :redirect_if_analyzed, only: %i[create]
-  before_action :set_web_page, only: %i[create]
+  before_action :create_web_page, only: %i[create]
+  before_action :set_analysis, except: %i[create]
 
   def create
-    analysis = @web_page.initiate_analysis
-    redirect_to analysis_path(analysis)
+    @web_page.initiate_analysis
+    redirect_to analysis_path(@web_page.analysis)
   end
 
   def show
-    @analysis = Analysis.find(params[:id])
+  end
+
+  def update
+    @analysis.web_page.initiate_analysis
+    redirect_to analysis_path(@analysis)
   end
 
   private
@@ -21,12 +26,15 @@ class AnalysesController < ApplicationController
     web_page = WebPage.find_by(url: analysis_params[:url])
 
     if web_page
-      latest_analysis = web_page.analyses.order(created_at: :desc).first!
-      redirect_to analysis_path(latest_analysis)
+      redirect_to analysis_path(web_page.analysis)
     end
   end
 
-  def set_web_page
+  def create_web_page
     @web_page = WebPage.create!(url: analysis_params[:url])
+  end
+
+  def set_analysis
+    @analysis = Analysis.find(params[:id])
   end
 end
--- app/models/web_page.rb
+++ app/models/web_page.rb
@@ -1,14 +1,17 @@
 class WebPage < ApplicationRecord
-  has_many :analyses, dependent: :destroy
+  has_one :analysis, dependent: :destroy
+  after_create :create_analysis
 
   validates :url, presence: true, uniqueness: true
   validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "must be a valid URL" }
 
   def initiate_analysis
-    ActiveRecord::Base.transaction do
-      new_analysis = self.analyses.create!
-      AnalysisJob.perform_later(new_analysis)
-      new_analysis
-    end
+    AnalysisJob.perform_later(self.analysis)
+  end
+
+  private
+
+  def create_analysis
+    Analysis.create!(web_page: self)
   end
 end
