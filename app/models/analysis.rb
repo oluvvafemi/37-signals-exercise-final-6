8d914fdd2a8a540d8c9eac76bd104de2c7794ef5
Chris Adebiyi <oluvvafemi@gmail.com>
2025-05-20 02:47:01 +0200

    Extract broadcast concern

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,26 +1,10 @@
 class Analysis < ApplicationRecord
+  include Executable, Broadcastable
+
   belongs_to :web_page
-  include Executable
 
   serialize :table_of_contents, coder: JSON
   serialize :top_word_frequencies, coder: JSON
 
-  enum :status, %w[
-    pending
-    fetching
-    parsing
-    analyzing
-    failed
-    completed
-  ].index_by(&:itself), default: :pending
-
-  after_update_commit :broadcast_update, if: -> { saved_change_to_status? }
-
-  private
-
-  def broadcast_update
-    broadcast_replace target: "analysis_details",
-                     partial: "analyses/analysis",
-                     locals: { analysis: self }
-  end
+  enum :status, %w[ pending fetching parsing analyzing failed completed ].index_by(&:itself), default: :pending
 end
---
+++ app/models/analysis/broadcastable.rb
@@ -0,0 +1,15 @@
+module Analysis::Broadcastable
+  extend ActiveSupport::Concern
+
+  included do
+    after_update_commit :broadcast_update, if: -> { saved_change_to_status? }
+  end
+
+  private
+
+  def broadcast_update
+    broadcast_replace target: "analysis_details",
+                     partial: "analyses/analysis",
+                     locals: { analysis: self }
+  end
+end
